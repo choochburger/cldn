8ef0ca944839c053ed9f4f40b74db00f3a7063a7
Chris Lyons <lyons.chr@gmail.com>
2011-04-30 19:36:22 -0400

    got basic jplayer stuff happening

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,10 @@ var cldn = (function() {
   
 $(document).ready(function() {
   initDescriptions();
+  initJPlayer();
 });
 
 function initDescriptions() {
-  /*
-  $('.description').each(function(i) {
-    $(this).delay(100).fadeOut((i+1) * 75, function() {
-      $(this).find('.invisible').removeClass('invisible'); // show text once fade in completes
-    });
-  });
-  */
 
   $('.contentItem').hover(
     function() {
@@ -21,5 +15,20 @@ function initDescriptions() {
       $(this).find('.description').fadeOut(400);
     });
 }
+
+function initJPlayer() {
+ $('#player').jPlayer( {
+    ready: function() {
+      $(this).jPlayer('setMedia', {
+        mp3: 'http://www.chrislyons.net/music/brothers_banded.mp3'
+      });
+      
+      console.log('ready');
+    }
+ });
+}
+
+
+
 
 })();
