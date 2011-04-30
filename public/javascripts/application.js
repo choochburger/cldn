26b5bc96dbc8e4656aafc77ec02c2d571677f737
Chris Lyons <lyons.chr@gmail.com>
2011-04-30 17:54:17 -0400

    added rollover scripts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var cldn = (function() {
+  
+$(document).ready(function() {
+  initDescriptions();
+});
+
+function initDescriptions() {
+  /*
+  $('.description').each(function(i) {
+    $(this).delay(100).fadeOut((i+1) * 75, function() {
+      $(this).find('.invisible').removeClass('invisible'); // show text once fade in completes
+    });
+  });
+  */
+
+  $('.contentItem').hover(
+    function() {
+      $(this).find('.description').fadeIn(500);
+    },
+    function() {
+      $(this).find('.description').fadeOut(400);
+    });
+}
+
+})();
