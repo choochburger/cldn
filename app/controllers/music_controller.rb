a1d8195bd0d9c52917dcf440b9fc78928303f2ee
Chris Lyons <lyons.chr@gmail.com>
2011-05-01 17:31:17 -0400

    update gitignore

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,5 +1,14 @@
 class MusicController < ApplicationController
   def show
-  end
+    @items = ContentItem.by_music_only 
 
+    # expose data to view via json. really don't need the width/height except
+    # for flash content. any way to only show for those?
+
+    @items_json = @items.map {|item| { 
+                                :url => item.asset_url,
+                                :type => item.kind
+                              }
+                            }.to_json
+  end
 end
