53852cf1966e6d373e8a43e03bdbb6f5abb2e55f
Chris Lyons <lyons.chr@gmail.com>
2011-05-01 20:48:06 -0400

    fixed heroku model issue

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -1,11 +1,10 @@
 class ContentItem < ActiveRecord::Base
   def self.by_web_only
-    #where('category = "web"')
     where :category => 'web'
   end
 
   def self.by_music_only
-    where('category = "music"')
+    where :category = 'music'
   end
 
   # could pass in a param for the data we want... not sure if this even makes sense
