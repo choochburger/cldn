eeb26aa5a46fedc22ee9495328e36523519ef8e6
Chris Lyons <lyons.chr@gmail.com>
2011-05-01 20:52:45 -0400

    oops, forgot top of hash rocket

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -4,7 +4,7 @@ class ContentItem < ActiveRecord::Base
   end
 
   def self.by_music_only
-    where :category = 'music'
+    where :category => 'music'
   end
 
   # could pass in a param for the data we want... not sure if this even makes sense
