79b9085986614405396628788bc5a26c4c932ebb
Chris Lyons <lyons.chr@gmail.com>
2011-08-14 15:15:33 -0400

    skull animations

--- public/javascripts/intro.js
+++ public/javascripts/intro.js
@@ -9,8 +9,21 @@ $(document).ready(function() {
 var cldn = {
 
   init: function() {
-  }
+    this.showSkull($('#music-skull'));
+    this.showSkull($('#web-skull'), 320);
+  },
 
+  showSkull: function(el, dly) {
+    $(el).delay(dly).animate({top: '20%'}, 1000, function() {
+      $(this).effect('bounce', {top: '15%'}, 400);
+    });
+
+    $(el).mouseover(function() {
+      $(this).find('.bubble').show();
+    }).mouseout(function() {
+      $(this).find('.bubble').hide();
+    });
+  }
 };
 
 })(window);
