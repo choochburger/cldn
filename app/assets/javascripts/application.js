14dd7c286d7f5567e7023880c96efcf0f6b264aa
Chris Lyons <lyons.chr@gmail.com>
2012-06-19 19:29:07 +0700

    fleshing out tile layout
    still very static

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,118 +1,39 @@
 //= require jquery
 //= require jquery_ujs
+//= require ember-0.9.8.1.min
 //= require_self
 //= require_tree .
 
-/*
-(function() {
+var App;
 
-var _data;
+$(function() {
+  App = Em.Application.create();
 
-$(document).ready(function() {
-  App.initDescriptions();
-  App.initJPlayer();
-  Shadowbox.init();
+  App.Router = Ember.Router.extend({
+    root: Ember.State.extend({
+      index: Ember.State.extend({
+        route: '/',
+        redirectsTo: 'web'
+      }),
+      web: Ember.State.extend({
+        route: '/web'
+      }),
+      music: Ember.State.extend({
+        route: '/music'
+      })
+    })
+  });
+
+  // add some tiles. temp for styling
+  var tilesView = Ember.View.create({
+    templateName: 'tiles',
+    tiles: cldn.data.web
+  });
+  tilesView.appendTo('#main');
+
 });
 
-var App = {
-
-  initDescriptions: function() {
-
-    $('.contentItem').hover(
-      function() {
-        $(this).find('.description').fadeIn(500);
-      },
-      function() {
-        $(this).find('.description').fadeOut(400);
-      });
-
-    $('.description').click(
-      function() {
-        var id = $(this).attr('id');
-        var pattern = /[0-9]+/g
-        var index = pattern.exec(id);
-        var path = _data[index].url;
-        var types = { image:'img', video: 'qt', flash: 'swf', audio: 'jp', link: 'iframe' };
-        var playerType = types[_data[index].type];
-
-        // audio opens in jplayer. all else is in a shadowbox
-        if (playerType == types.audio) {
-          $('.jp-audio').fadeIn();
-          $('#player').jPlayer("setMedia", {mp3: path}).jPlayer('play');
-          $('#song-title').text(_data[index].title);
-        } else if (playerType == types.image) {
-          // shadowbox can auto detect with and height, so not needed here
-          Shadowbox.open({ content: path, player: playerType });
-        } else {
-          var width  = _data[index].width;
-          var height = _data[index].height;
-
-          Shadowbox.open({ content: path, player: playerType, width: width, height: height});
-        }
-
-        // stop and hide jPlayer if content is video or swf
-        if (playerType == types.video ||
-            playerType == types.flash) {
-            $('#player').jPlayer('stop');
-            $('.jp-audio').fadeOut();
-        }
-      }
-    );
-  },
-
-  initJPlayer: function () {
-   $('#player').jPlayer( {
-      ready: function() {
-      },
-
-      play: function() {
-        cldn.checkPlayerScroll();
-      },
-
-      pause: function() {
-        $('.jp-audio').fadeOut();
-      },
-
-      swfPath: 'javascripts/jplayer',
-      solution: 'html, flash',
-
-      error: function(e) {
-        console.log(e.jPlayer.error);
-      }
-   });
-
-   // watch for scroll and lock player to the top if player is out of view
-   $(window).scroll(function () {
-     cldn.checkPlayerScroll();
-   });
-
-  },
-
-  checkPlayerScroll: function () {
-     var jpa = $('.jp-audio');
-     var scroll = $(window).scrollTop();
-
-     // fix player to top of window if start scrolling past it
-     // or click a song when the player is out of view
-     //
-     if (scroll > jpa.height()/3 + jpa.offset().top) {
-        jpa.data('origTop', jpa.offset().top);
-        jpa.addClass('sticky-player');
-      } else if (jpa.hasClass('sticky-player') &&
-                 scroll < jpa.data('origTop')) {
-        jpa.removeClass('sticky-player');
-      }
-  },
-
-  data: function (d) {
-    _data = d;
-  },
-
-  getData: function() {
-    return _data;
-  }
-
-};
-
-})();
-*/
+Handlebars.registerHelper('createBgUrl', function(prop) {
+  var path = Ember.getPath(this, prop);
+  return 'background-image: url(\''+path+'\');';
+});
