2782ecb4cb2bd29ac7d3c12865bccdf43597269f
Chris Lyons <lyons.chr@gmail.com>
2012-06-25 13:29:58 +0700

    shuffle namespaces and loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,56 +11,23 @@ $(function() {
 
   App = Em.Application.create({rootElement: '#main'});
 
-  rootRoutes = App.scaffold({
+  rootRoutes = cldn.scaffold(App, 'dev', {
     'Application': {}, 'Nav': {}, 'Sandbox': {}, 'About': {}, 'Contact': {},
     'Dev':   {
       templateName: 'tiles',
-      context: {'tiles': cldn.data.web}
+      context: {'tiles': cldn.data.dev}
     },
     'Music': {
       templateName: 'tiles',
       context: {'tiles': cldn.data.music}
     },
-  }, 'dev');
+  });
 
   App.Router = Em.Router.extend({
     location: 'hash',
     enableLogging: false,
     root: Em.Route.extend(rootRoutes)
   });
-
-  // @param {Object} views  An object containing view names as keys with optional templateName and context params tacked on
-  // @param {String} indexRoute
-  // @return {Object} Returns an object containing all root routes
-  //
-  // App.scaffold creates:
-  //   * App.NameController for all keys
-  //   * App.NameView (using template 'name') for all keys
-  //   * Root route to 'name' for all keys
-  //
-  App.scaffold = function(views, indexRoute) {
-    routes = {
-      index: Em.Route.extend({
-        route: '/',
-        redirectsTo: indexRoute
-      })
-    };
-
-    $.each(views, function(name, data) {
-      var lowerCaseName = name.toLowerCase();
-
-      App[name+'Controller'] = Em.Controller.extend(data.context || {});
-      App[name+'View'] = Em.View.extend({
-        templateName: data.templateName || lowerCaseName
-      });
-      routes[lowerCaseName] = Em.Route.extend({
-        route: '/'+lowerCaseName,
-        connectOutlets: function(router, context) { App.loadView(router, lowerCaseName); }
-      });
-    });
-
-    return routes;
-  }
 
   App.loadView = function(router, name) {
     var $el = $('#content'),
@@ -85,3 +52,40 @@ Handlebars.registerHelper('createBgUrl', function(prop) {
   var path = Ember.getPath(this, prop);
   return 'background-image: url(\''+path+'\');';
 });
+
+(function() {
+  // @param  {Object} app  A reference to the ember app to attach to
+  // @param  {String} indexRoute
+  // @param  {Object} views  An object containing view names as keys with optional templateName and context params tacked on
+  // @return {Object} Returns an object containing all root routes
+  //
+  // cldn.scaffold creates:
+  //   * App.NameController for all keys
+  //   * App.NameView (using template 'name') for all keys
+  //   * Root route to 'name' for all keys
+  //
+  cldn.scaffold = function(app, indexRoute, views) {
+    routes = {
+      index: Em.Route.extend({
+        route: '/',
+        redirectsTo: indexRoute
+      })
+    };
+
+    $.each(views, function(name, data) {
+      var lowerCaseName = name.toLowerCase();
+
+      app[name+'Controller'] = Em.Controller.extend(data.context || {});
+      app[name+'View'] = Em.View.extend({
+        templateName: data.templateName || lowerCaseName
+      });
+      routes[lowerCaseName] = Em.Route.extend({
+        route: '/'+lowerCaseName,
+        connectOutlets: function(router, context) { app.loadView(router, lowerCaseName); }
+      });
+    });
+
+    return routes;
+  };
+
+})();
