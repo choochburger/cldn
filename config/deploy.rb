563276eeb075454cdbc98d9ec72c579fec3b8777
Chris Lyons <lyons.chr@gmail.com>
2012-08-20 23:58:00 -0400

    another test

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,8 @@ set :deploy_via, :export
 
 ssh_options[:keys] = %w(/Users/chrislyons/.ssh/id_rsa)
 
+default_run_options[:pty] = true
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
