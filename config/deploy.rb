a8605892608d592427ba2a739797d1074dba4c25
Chris Lyons <lyons.chr@gmail.com>
2012-08-25 11:53:24 -0400

    precompile locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,12 +36,25 @@ namespace :deploy do
     run "touch #{current_release}/tmp/restart.txt"
   end
 
-  task :stop, :roles => :app do
-    # Do nothing.
+  desc "precompile locally"
+  task :precompile do
+    run_locally("rm -rf public/assets/*")
+    run_locally("bundle exec rake assets:precompile")
+    servers = find_servers_for_task(current_task)
+    port_option = port ? " -e 'ssh -p #{port}' " : ''
+    servers.each do |server|
+      run_locally("rsync --recursive --times --rsh=ssh --compress --
+human-readable #{port_option} --progress public/assets #{user}
+@#{server}:#{shared_path}")
+     end
   end
 
   desc "reload the database with seed data"
   task :seed do
     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
   end
 end
